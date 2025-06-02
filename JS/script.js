eb01bdcbc00d1841c1fa118103b56a430411edfe
Herbert <20240101@ricaldone.edu.sv>
2025-06-02 12:09:59 -0600

    funcion para insertar pendiente

--- JS/script.js
+++ JS/script.js
@@ -40,3 +40,59 @@ function MostrarDatos(datos){
 
 //Lllamada inicial para que se carguen los datos que vienen del servidor
 obtenerPersonas();
+
+
+
+
+//Agregar un nuevo registro
+
+const modal = document.getElementById("modal-agregar");
+const btnAgregar = document.getElementById("btnAbrirModal");
+const btnCerrar = document.getElementById("btnCerrarModal");
+
+btnAgregar.addEventListener("click", () => {
+    modal.showModal(); //Abrir el modal
+});
+
+btnCerrar.addEventListener("click", () => {
+    modal.close();
+})
+
+
+
+document.getElementById("frmAgregar").addEventListener("submit", async e=> {
+    e.preventDefault(); //"e" representa al Evento Submit, evita que el formulario se envie de golpe
+
+    //Capturar los valores del formulario
+    const nombre = document.getElementById("Nombre").value.trim();
+    const apellido = document.getElementById("Apellido").value.trim();
+    const email = document.getElementById("Email").value.trim();
+    const edad = document.getElementById("Edad").value.trim();
+
+    //Validadicion basica
+    if(!nombre || !apellido || !email || !edad){
+        alert("Complete todos los campos");
+        return;
+    }
+
+    //Llamar a la API para enviar al usuario
+
+    const respuesta = await fetch(API_URL, {
+        method: "POST",
+        headers: {'Content-Type': 'Application/json'},
+        body: JSON.stringify({nombre, apellido, email, edad})
+    });
+
+    if(respuesta.ok){
+        alert("El registro fue agregado correctamente")
+
+        document.getElementById("frmAgregar").reset();
+        modal.close();
+
+        obtenerPersonas();
+    } else {
+        alert("Hubo un error al agregar")
+    }
+
+
+});
